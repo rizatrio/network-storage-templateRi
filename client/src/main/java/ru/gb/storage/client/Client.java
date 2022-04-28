ec7ce02b0ca6c04e2c6b891ce31892bfeb358d76
Rizat19 <e-rizat.orazalina@homecredit.kz>
2022-04-28 16:57:22 +0600

    Урок 2. Netty. Основы 1. Подготовьте echo-server, который возвращает ответ только после получения символа переноса строки.

--- client/src/main/java/ru/gb/storage/client/Client.java
+++ client/src/main/java/ru/gb/storage/client/Client.java
@@ -1,14 +1,69 @@
 package ru.gb.storage.client;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
 
-public class Client extends Application {
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Client {
+
     public static void main(String[] args) {
-        Application.launch(args);
+        new Client().start();
     }
 
-    public void start(Stage stage) throws Exception {
-        stage.show();
+    public void start() {
+        final NioEventLoopGroup group = new NioEventLoopGroup(1);
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                            System.out.println("Echo message: ");
+                                            final ByteBuf m = (ByteBuf) msg;
+                                            while (m.isReadable()) {
+                                                System.out.print((char) m.readByte());
+                                            }
+                                            System.out.flush();
+                                            System.out.println();
+                                            ReferenceCountUtil.release(msg);
+                                        }
+                                    }
+                            );
+                        }
+                    });
+
+            System.out.println("Client started...");
+
+            Channel channel = bootstrap.connect("localhost", 9000).sync().channel();
+
+            while (channel.isActive()) {
+                Scanner scanner = new Scanner(System.in);
+                String message = scanner.nextLine();
+
+                ByteBuf msg = Unpooled.wrappedBuffer((message + "\t" + new Date()).getBytes(StandardCharsets.UTF_8));
+                channel.write(msg);
+                channel.flush();
+            }
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
     }
 }
--- server/src/main/java/ru/gb/storage/server/Server.java
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -1,7 +1,108 @@
 package ru.gb.storage.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.charset.StandardCharsets;
+
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Server started");
+    private final int port;
+    StringBuilder message = new StringBuilder();  // стрингбилдер для склеивания нашего сообшения для отправки клиенту
+
+    public static void main(String[] args) throws InterruptedException {
+        new Server(9000).start();
+    }
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+
+                                        @Override
+                                        public void channelRegistered(ChannelHandlerContext ctx) {
+                                            System.out.println("channelRegistered");
+                                        }
+
+                                        @Override
+                                        public void channelUnregistered(ChannelHandlerContext ctx) {
+                                            System.out.println("channelUnregistered");
+                                        }
+
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) {
+                                            System.out.println("channelActive");
+                                        }
+
+                                        @Override
+                                        public void channelInactive(ChannelHandlerContext ctx) {
+                                            System.out.println("channelInactive");
+                                        }
+
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                            System.out.println("channelRead");
+                                            System.out.println("Received message: ");
+                                            final ByteBuf m = (ByteBuf) msg;
+
+                                            for (int i = m.readerIndex(); i < m.writerIndex(); i++) {
+                                                System.out.print((char) m.getByte(i));
+                                                message.append((char) m.getByte(i));
+                                            }
+                                            message.append("\n");
+                                            m.clear();
+                                            System.out.flush();
+                                            System.out.println();
+
+                                            char[] ch = new char[message.length()];
+                                            message.toString().getChars(0, message.length(), ch, 0);
+
+                                            for (int i = 0; i < ch.length - 1; i++) {
+                                                if ((byte) ch[i] == 92 && (byte) ch[i + 1] == 110) {
+                                                    message.delete(message.length() - 2, message.length());
+                                                    ctx.writeAndFlush(((ByteBuf) msg).writeBytes(message.toString().getBytes(StandardCharsets.UTF_8)));
+                                                    message.delete(0, message.length());
+                                                }
+                                            }
+                                        }
+
+                                        @Override
+                                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                            System.out.println("Cause exception");
+                                            cause.printStackTrace();
+                                            ctx.close();
+                                        }
+                                    }
+
+                            );
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            Channel channel = server.bind(port).sync().channel();
+
+            System.out.println("Server started");
+            channel.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
